2c089b3b2c1ae643d85bc4f7eb852395a97c5daa
Serhat <mserhatkaraatay@gmail.com>
2024-10-31 02:33:08 +0300

    Update resimdenbolge.py

--- sifirdan_goruntu/resimdenbolge.py
+++ sifirdan_goruntu/resimdenbolge.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np
-cocukresmi=cv2.imread("23nisan.jpg")
-cv2.imshow("Cocuklar",cocukresmi)
+cocuk_resmi=cv2.imread("23nisan.jpg")
+print("Resim Özellikleri : " , cocuk_resmi.shape)
+kesit=cocuk_resmi[50:400,250:550]#y,x koordinatları
+cocuk_resmi[0:350,100:400]=kesit  #kesilen parçayı resimin bir bölgesine yapıştırma işlemi
+#kesiti aldığımız pikseller ve yapıştırılan kısım uyuşmalı
+cocuk_resmi[ : , : ,0]=255 #resmin mavi kısmını beyaz yapma(0. kanal blue olduğu için mavi kanala işlem gerçekleşti)
+cocuk_resmi[400:500,300:350]=(50,200,150)#resmi belirlenen alanına renk uyguladık
+cv2.imshow("Cocuklar : ",cocuk_resmi)
+#cv2.imshow("Cocuklar",kesit)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
